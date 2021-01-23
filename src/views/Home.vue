58e7b380ae6b9a41f1a480ed0cdcd8641b95f28e
sixpeteunder <plenjo15@gmail.com>
2021-01-23 13:52:48 +0300

    Bring branch up to date with current project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ul>
+      <li v-for="item in items" v-bind:key="item.id">
+        {{ item.name }}
+      </li>
+    </ul>
+
+    <button
+      class="btn btn-link"
+      v-if="pageInfo.prev"
+      v-on:click="fetchCharacters(pageInfo.prev)"
+      :disabled="loading"
+    >
+      Prev
+    </button>
+    <button
+      class="btn btn-link"
+      v-if="pageInfo.next"
+      v-on:click="fetchCharacters(pageInfo.next)"
+      :disabled="loading"
+    >
+      Next
+    </button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+const axios = require("axios");
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      loading: false,
+      items: [],
+      pageInfo: {}
+    };
+  },
+  created: function() {
+    this.fetchCharacters('https://rickandmortyapi.com/api/character')
+  },
+  methods: {
+    fetchCharacters(url) {
+      this.loading = true;
+      let self = this;
+
+      axios
+        .get(url)
+        .then(function(response) {
+          self.items = response.data.results;
+          self.pageInfo = response.data.info;
+        })
+        .catch(function(error) {
+          alert("Something went wrong. Please check your internet connection.");
+          console.log(error);
+        })
+        .then(function() {
+          self.loading = false;
+        });
+    }
   }
 };
+
+// vm.fetchCharacters('https://rickandmortyapi.com/api/character')
 </script>
