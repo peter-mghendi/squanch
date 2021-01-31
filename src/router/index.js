d5b622ec44c4414f0126cefee7b8b4043771d7f3
sixpeteunder <plenjo15@gmail.com>
2021-01-31 07:18:31 +0300

    Switch to HTML5 history navigation mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 
 const routes = [
@@ -67,7 +67,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 });
 
