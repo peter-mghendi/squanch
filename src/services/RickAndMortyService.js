ee45ac9b53b8e5dc01633c94fcc24e68b3cd3024
sixpeteunder <plenjo15@gmail.com>
2021-02-07 08:58:43 +0300

    Migrate list pages to service layer

--- src/services/RickAndMortyService.js
+++ src/services/RickAndMortyService.js
@@ -1,80 +1,85 @@
-const axios = require = ("axios");
+const axios = require("axios");
 const apiUrl = "https://rickandmortyapi.com/api";
 
 export class RickAndMortyService {
-    static getCharacters(ids = []) {
-        let url = `${apiUrl}/character`;
-        if (Array.isArray(ids) && ids.length > 0) {
-            url += `/${ids.join()}`;
-        }
-
-        return axios.get(url);
+  static getCharacters(ids = [], page = 1) {
+    let url = `${apiUrl}/character`;
+    if (Array.isArray(ids) && ids.length > 0) {
+      url += `/${ids.join()}`;
     }
 
-    static getLocations(ids = []) {
-        let url = `${apiUrl}/location`;
-        if (Array.isArray(ids) && ids.length > 0) {
-            url += `/${ids.join()}`;
-        }
+    let options = null;
+    if (page > 1) options = { params: { page: page } };
+    return axios.get(url, options);
+  }
 
-        return axios.get(url);
+  static getLocations(ids = [], page = 1) {
+    let url = `${apiUrl}/location`;
+    if (Array.isArray(ids) && ids.length > 0) {
+      url += `/${ids.join()}`;
     }
 
-    static getEpisodes(ids = []) {
-        let url = `${apiUrl}/episode`;
-        if (Array.isArray(ids) && ids.length > 0) {
-            url += `/${ids.join()}`;
-        }
+    let options = null;
+    if (page > 1) options = { params: { page: page } };
+    return axios.get(url, options);
+  }
 
-        return axios.get(url);
+  static getEpisodes(ids = [], page = 1) {
+    let url = `${apiUrl}/episode`;
+    if (Array.isArray(ids) && ids.length > 0) {
+      url += `/${ids.join()}`;
     }
 
-    static getCharacter(id) {
-        if (!Number.isInteger(id)) {
-            throw new Error("wrong parameter type");
-        }
+    let options = null;
+    if (page > 1) options = { params: { page: page } };
+    return axios.get(url, options);
+  }
 
-        return axios.get(`${apiUrl}/character/${id}`)
-            .then(response => {
-                let data = response.data;
-                this.getEpisodes(this.#extractIds(data.episode))
-                    .then(response =>
-                        data.episodes = Array.isArray(response.data)
-                            ? response.data : [response.data]);
-            });
+  static getCharacter(id) {
+    if (!Number.isInteger(id)) {
+      throw new Error("wrong parameter type");
     }
 
-    static getLocation(id) {
-        if (!Number.isInteger(id)) {
-            throw new Error("wrong parameter type");
-        }
+    return axios.get(`${apiUrl}/character/${id}`).then(response => {
+      let data = response.data;
+      this.getEpisodes(this.extractIds(data.episode)).then(
+        response =>
+          (data.episodes = Array.isArray(response.data)
+            ? response.data
+            : [response.data])
+      );
+    });
+  }
 
-        return axios.get(`${apiUrl}/location/${id}`)
-            .then(response => {
-                let data = response.data;
-                this.getCharacters(this.#extractIds(data.residents))
-                    .then(response =>
-                        data.residents = Array.isArray(response.data) ?
-                            response.data : [response.data]);
-            });
+  static getLocation(id) {
+    if (!Number.isInteger(id)) {
+      throw new Error("wrong parameter type");
     }
 
-    static getEpisode(id) {
-        if (!Number.isInteger(id)) {
-            throw new Error("wrong parameter type");
-        }
+    return axios.get(`${apiUrl}/location/${id}`).then(response => {
+      let data = response.data;
+      this.getCharacters(this.extractIds(data.residents)).then(
+        response =>
+          (data.residents = Array.isArray(response.data)
+            ? response.data
+            : [response.data])
+      );
+    });
+  }
 
-        return axios.get(`${apiUrl}/episode/${id}`)
-            .then(response => {
-                let data = response.data;
-                this.getCharacters(this.#extractIds(data.characters))
-                    .then(response =>
-                        data.characters = Array.isArray(response.data)
-                            ? response.data : [response.data]);
-            });
+  static getEpisode(id) {
+    if (!Number.isInteger(id)) {
+      throw new Error("wrong parameter type");
     }
 
-    static #extractIds(urls) {
-        return urls.map(x => x.split("/").slice(-1)[0]);
-    }
+    return axios.get(`${apiUrl}/episode/${id}`).then(response => {
+      let data = response.data;
+      this.getCharacters(this.extractIds(data.characters)).then(
+        response =>
+          (data.characters = Array.isArray(response.data)
+            ? response.data
+            : [response.data])
+      );
+    });
+  }
 }
